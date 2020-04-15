195eac9bd2d5b3ec9b4fd7fe518d6b0f8fa5f5c4
Thibault Manfredi <thibault.manfredi@epitech.eu>
2020-04-15 11:49:21 +0200

    Update dag.py

--- dag.py
+++ dag.py
@@ -3,17 +3,23 @@ from datetime import datetime , timedelta
 import airflow
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+
 default_args = {
- ‘owner’: ‘Airflow’ ,
- ‘depends_on_past’: False ,
- ‘start_date’: airflow.utils.dates.days_ago(2) ,
- ‘retries’: 1 ,
- ‘retry_delay’: timedelta(minutes=1) ,
+ ‘owner’: ‘airflow’,
+ ‘depends_on_past’: False,
+ ‘start_date’: airflow.utils.dates.days_ago(2),
+ ‘retries’: 1,
+ ‘retry_delay’: timedelta(minutes=1),
 }
+
 dag = DAG(
- ‘dummy_try1’ ,
- default_args=default_args ,
- schedule_interval=None)
-for i in range(50000):
-    tasks = DummyOperator(task_id=’{}’.format(i),dag=dag,
-                        pool=’default_pool)
+ ‘dummy_try1’,
+ default_args=default_args,
+ description='dummy test',
+ schedule_interval=None
+)
+
+with dag:
+  for i in range(50000):
+      tasks = DummyOperator(task_id=’{}’.format(i),dag=dag,
+                          pool=’default_pool)
