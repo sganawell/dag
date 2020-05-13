d8fa7ca808f64d9d041a32f9329cd1fa70c296fe
Thibault Manfredi <thibault@flexper.fr>
2020-05-14 00:57:20 +0200

    another test

--- dag_kub_operator.py
+++ dag_kub_operator.py
@@ -5,123 +5,46 @@ from airflow.contrib.kubernetes.secret import Secret
 from airflow.contrib.kubernetes.volume import Volume
 from airflow.contrib.kubernetes.volume_mount import VolumeMount
 from airflow.contrib.kubernetes.pod import Port
+from airflow.operators.dummy_operator import DummyOperator
 
-
-secret_file = Secret('volume', '/etc/sql_conn', 'airflow-secrets', 'sql_alchemy_conn')
-secret_env  = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
-secret_all_keys  = Secret('env', None, 'airflow-secrets-2')
-volume_mount = VolumeMount('test-volume',
-                            mount_path='/root/mount_file',
-                            sub_path=None,
-                            read_only=True)
-port = Port('http', 80)
-configmaps = ['test-configmap-1', 'test-configmap-2']
-
-volume_config= {
-    'persistentVolumeClaim':
-      {
-        'claimName': 'test-volume'
-      }
-    }
-volume = Volume(name='test-volume', configs=volume_config)
-
-init_container_volume_mounts = [k8s.V1VolumeMount(
-  mount_path='/etc/foo',
-  name='test-volume',
-  sub_path=None,
-  read_only=True
-)]
-
-init_environments = [k8s.V1EnvVar(
-  name='key1',
-  value='value1'
-), k8s.V1EnvVar(
-  name='key2',
-  value='value2'
-)]
-
-init_container = k8s.V1Container(
-  name="init-container",
-  image="ubuntu:16.04",
-  env=init_environments,
-  volume_mounts=init_container_volume_mounts,
-  command=["bash", "-cx"],
-  args=["echo 10"]
-)
-
-affinity = {
-    'nodeAffinity': {
-      'preferredDuringSchedulingIgnoredDuringExecution': [
-        {
-          "weight": 1,
-          "preference": {
-            "matchExpressions": {
-              "key": "disktype",
-              "operator": "In",
-              "values": ["ssd"]
-            }
-          }
-        }
-      ]
-    },
-    "podAffinity": {
-      "requiredDuringSchedulingIgnoredDuringExecution": [
-        {
-          "labelSelector": {
-            "matchExpressions": [
-              {
-                "key": "security",
-                "operator": "In",
-                "values": ["S1"]
-              }
-            ]
-          },
-          "topologyKey": "failure-domain.beta.kubernetes.io/zone"
-        }
-      ]
-    },
-    "podAntiAffinity": {
-      "requiredDuringSchedulingIgnoredDuringExecution": [
-        {
-          "labelSelector": {
-            "matchExpressions": [
-              {
-                "key": "security",
-                "operator": "In",
-                "values": ["S2"]
-              }
-            ]
-          },
-          "topologyKey": "kubernetes.io/hostname"
-        }
-      ]
-    }
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
 }
 
-tolerations = [
-    {
-        'key': "key",
-        'operator': 'Equal',
-        'value': 'value'
-     }
-]
+dag = DAG(
+    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
-k = KubernetesPodOperator(namespace='default',
-                            image="ubuntu:16.04",
-                            cmds=["bash", "-cx"],
-                            arguments=["echo", "10"],
-                            labels={"foo": "bar"},
-                            secrets=[secret_file, secret_env, secret_all_keys],
-                            ports=[port],
-                            volumes=[volume],
-                            volume_mounts=[volume_mount],
-                            name="test",
-                            task_id="task",
-                            affinity=affinity,
-                            is_delete_operator_pod=True,
-                            hostnetwork=False,
-                            tolerations=tolerations,
-                            configmaps=configmaps,
-                            init_containers=[init_container],
-                            priority_class_name="medium",
-                            )
+
+start = DummyOperator(task_id='run_this_first', dag=dag)
+
+passing = KubernetesPodOperator(namespace='default',
+                          image="Python:3.6",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="passing-test",
+                          task_id="passing-task",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+failing = KubernetesPodOperator(namespace='default',
+                          image="ubuntu:1604",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="fail",
+                          task_id="failing-task",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+passing.set_upstream(start)
+failing.set_upstream(start)
